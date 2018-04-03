6ffe8f52a9d216925f41550d27360c4846da3b75
Yücel YILMAZ <yucelyilmaz07@gmail.com>
2018-04-03 20:28:42 +0300

    Add files via upload

---
+++ Cryptography/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Cryptography
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(55, 55);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(601, 225);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 27);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(593, 194);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Encrypt";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(452, 84);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 29);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Copy Text";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(452, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Encrypted Text :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(188, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(452, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Text you wanna encrypt : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(188, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 27);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(593, 194);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Decrypt";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(452, 84);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 29);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Copy Text";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(452, 122);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 29);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(188, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(213, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Decrypted Text :";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(452, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Text you wanna decrypt : ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(188, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(213, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(709, 346);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Open Sans Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cryptography";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+
+    }
+}
+
---
+++ Cryptography/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cryptography
+{
+    public partial class Form1 : Form
+    {
+        string text,finalText,binaryTotal;
+        byte binaryFirst,binarySecond,notBinaryFirst;
+        char[] charList;
+        char[] addList = {'Y','U','C','E','L'};
+        char[] mod;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            encryptText();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decryptText();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void encryptText()
+        {
+            text = textBox1.Text;
+            finalText = "";
+            charList = text.ToCharArray();
+            mod = charList;
+            for (int i = 0; i < charList.Length; i++)
+            {
+                if (Convert.ToInt16(charList[i]) == 32)
+                {
+                    finalText += " ";
+                    mod[i] = ' ';
+                }
+                else
+                {
+                    binaryFirst = Convert.ToByte(Convert.ToInt16(charList[i]));
+                    notBinaryFirst = unchecked((byte)(~binaryFirst)); //~binaryFirst;
+                    binarySecond = Convert.ToByte(Convert.ToInt16(addList[i % addList.Length]));
+                    binaryTotal = calcXOR(Convert.ToByte(notBinaryFirst), binarySecond);
+                    finalText += (char)((Convert.ToInt32(binaryTotal, 2) % 26) + 65);
+                    mod[i] = (char)(Convert.ToInt32(binaryTotal, 2) / 26 + 48);
+                }
+            }
+            textBox2.Text = finalText;
+        }
+
+        private void decryptText()
+        {   
+            text = textBox3.Text;
+            int temp;
+            finalText = "";
+            charList = text.ToCharArray();
+            for (int i = 0; i < charList.Length; i++)
+            {
+                if (Convert.ToInt32(mod[i]) == 32)
+                {
+                    finalText += " ";
+                }
+                else
+                {
+                    binaryFirst = Convert.ToByte(Convert.ToInt16(charList[i]));
+                    binarySecond = Convert.ToByte(Convert.ToInt16(addList[i % addList.Length]));
+                    temp = ((Convert.ToInt16(charList[i]) - 65) % 26) + ((Convert.ToInt32(mod[i]) - 48) * 26);
+                    binaryTotal = calcXOR(Convert.ToByte(temp), binarySecond);
+                    byte temp2 = unchecked((byte)(~Convert.ToByte(Convert.ToInt32(binaryTotal, 2))));
+                    finalText += (char)temp2;
+                }
+            }
+            textBox4.Text = finalText;
+        }
+
+        private void clearAll()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        public string calcXOR(byte a, byte b)
+        {
+            char[] charAArray = Convert.ToString(a,2).PadLeft(8,'0').ToCharArray();
+            char[] charBArray = Convert.ToString(b, 2).PadLeft(8, '0').ToCharArray();
+            char[] result = new char[8];
+            string control = "";
+            for (int i = 0; i < 8; i++)
+            {
+                control = charAArray[i].ToString();
+                control += charBArray[i].ToString();
+                if ( control == "01" || control == "10")
+                    result[i] = '1';
+                else
+                    result[i] = '0';
+            }
+            return new string(result);
+        }
+
+        public string calcBackXOR(byte a, byte b)
+        {
+            char[] charAArray = Convert.ToString(a, 2).PadLeft(8, '0').ToCharArray();
+            char[] charBArray = Convert.ToString(b, 2).PadLeft(8, '0').ToCharArray();
+            char[] result = new char[8];
+            string control = "";
+            for (int i = 0; i < 8; i++)
+            {
+                control = charAArray[i].ToString();
+                control += charBArray[i].ToString();
+                if (control == "00" ||control == "11")
+                    result[i] = '0';
+                else
+                    result[i] = '1';
+            }
+            return new string(result);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.CharacterCasing = CharacterCasing.Upper;
+            textBox2.CharacterCasing = CharacterCasing.Upper;
+            textBox3.CharacterCasing = CharacterCasing.Upper;
+            textBox4.CharacterCasing = CharacterCasing.Upper;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage2;
+            textBox3.Text = textBox2.Text;
+            textBox4.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedTab = tabPage1;
+            textBox1.Text = textBox4.Text;
+            textBox2.Text = "";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back ||  char.IsWhiteSpace(e.KeyChar));
+        }
+   } 
+}
